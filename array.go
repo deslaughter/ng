536d64e959ba4574e846c5c50bb31747f95c6149
Derek Slaughter <deslaughter@gmail.com>
2017-07-09 07:51:46 -0400

    Added doc.go file for documentation
    Moved package description from array.go to doc.go

--- array.go
+++ array.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package ng provides numerical methods for the Go programming language.
 package ng
 
 // Array is the base type of the ng package.
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 Derek Slaughter. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package ng provides numerical methods for the Go programming language.
+package ng
