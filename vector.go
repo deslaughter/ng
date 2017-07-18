e4caa619924601cf16e2ea7c3b1ae5368a44bbc6
Derek Slaughter <deslaughter@gmail.com>
2017-07-18 10:04:40 -0400

    Added documentation for Vector methods.

--- vector.go
+++ vector.go
@@ -11,18 +11,37 @@ import (
 // Vector is the base type of ng
 type Vector []float64
 
+// NewVector returns a vector with the given number of elements.
 func NewVector(size int) Vector {
 	return make([]float64, size)
 }
 
+// Matrix returns a 2-D view of the vector with the given number of rows and
+// columns. The vector is interpreted in row-major order. A panic will occur
+// if rows * columns > Vector.Size().
+func (v Vector) Matrix(rows, columns int) Matrix {
+	d := v[:rows*columns]
+	m := make([][]float64, rows)
+	for i := range m {
+		m[i], d = d[:columns:columns], d[columns:]
+	}
+	return m
+}
+
+// Dimensions returns the number of dimensions in the vector which is 1.
 func (v Vector) Dimensions() int {
 	return 1
 }
 
+// Size returns of slice of integers containing the length of each dimension
+// of the vector. Since there is only one dimension, the first element of
+// the slice will contain the same value as Vector.Size().
 func (v Vector) Size() []int {
 	return []int{len(v)}
 }
 
+// Dot returns the dot product of this vector and the given vector.
+// This function will panic if vn.Size() < v.Size().
 func (v Vector) Dot(vn Vector) float64 {
 	sum := 0.0
 	vt := vn[:len(v)]
@@ -32,6 +51,7 @@ func (v Vector) Dot(vn Vector) float64 {
 	return sum
 }
 
+// Magnitude returns the sum of each element squared.
 func (v Vector) Magnitude() float64 {
 	sum := 0.0
 	for i := range v {
@@ -56,25 +76,21 @@ func (v Vector) Add(vn Vector) {
 	}
 }
 
+// Normalize scales the vector by one over the magnitude of the vector such that
+// the vector's magnitude is one. Transforms vector into the unit vector.
 func (v Vector) Normalize() {
 	v.Scale(1 / v.Magnitude())
 }
 
-func (v Vector) Matrix(rows, columns int) Matrix {
-	d := v[:rows*columns]
-	m := make([][]float64, rows)
-	for i := range m {
-		m[i], d = d[:columns:columns], d[columns:]
-	}
-	return m
-}
-
+// Fill sets all elements of the vector to the given value.
 func (v Vector) Fill(value float64) {
 	for i := range v {
 		v[i] = value
 	}
 }
 
+// Resize changes the size and capacity of the vector. If the capacity is
+// less than size, a new vector is allocated and the values are copied into it.
 func (v *Vector) Resize(size int) {
 	if size <= cap(*v) {
 		*v = (*v)[:size:size]
