fd881b772364cd852f7c7783d7b9ca41549ccd12
Derek Slaughter <deslaughter@gmail.com>
2017-07-17 23:31:44 -0400

    Formatted and fixed variables in vector_test.go

--- vector_test.go
+++ vector_test.go
@@ -7,39 +7,29 @@ package ng
 import "testing"
 
 func TestNewVector(t *testing.T) {
-
 	const size = 5
-
-	a := NewVector(size)
-	if act, exp := len(a), size; act != exp {
+	v := NewVector(size)
+	if act, exp := len(v), size; act != exp {
 		t.Fatalf("a := NewVector(size); len(a) = %d, expected %d", act, exp)
 	}
 }
 
-func TestVector(t *testing.T) {
-
-}
-
-func TestMatrix(t *testing.T) {
-
-}
-
 func TestFill(t *testing.T) {
 	const value = 1.0
-	a := NewVector(5)
-	a.Fill(value)
-	for i := range a {
-		if a[i] != value {
-			t.Fatalf("a[i] = %f, expected %f", a[i], value)
+	v := NewVector(5)
+	v.Fill(value)
+	for i := range v {
+		if v[i] != value {
+			t.Fatalf("a[i] = %f, expected %f", v[i], value)
 		}
 	}
 }
 
 func TestResize(t *testing.T) {
 	const size = 10
-	a := NewVector(5)
-	a.Resize(size)
-	if act, exp := len(a), size; act != exp {
+	v := NewVector(5)
+	v.Resize(size)
+	if act, exp := len(v), size; act != exp {
 		t.Fatalf("a.Resize(size); len(a) = %d, expected %d", act, exp)
 	}
 }
@@ -50,7 +40,7 @@ func TestSum(t *testing.T) {
 	for i := range v {
 		v[i] = 1.0
 	}
-	if act, exp := v.Sum(), 10.0; act != exp {
+	if act, exp := v.Sum(), float64(len(v)); act != exp {
 		t.Fatalf("v.Resize(size); len(v) = %v, expected %v", act, exp)
 	}
 }
