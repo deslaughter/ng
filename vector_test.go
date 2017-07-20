cc852845c38ff50dbd3333cadf0cd8dc8c9d3270
Derek Slaughter <deslaughter@gmail.com>
2017-07-19 21:24:11 -0400

    Added TestVector_Matrix function

--- vector_test.go
+++ vector_test.go
@@ -17,6 +17,44 @@ func TestNewVector(t *testing.T) {
 	}
 }
 
+func TestVector_Matrix(t *testing.T) {
+
+	// Number of rows and columns in matrix
+	rows, columns := 2, 3
+
+	// Create vector
+	v := make(Vector, rows*columns)
+
+	// Fill vector with unique values
+	for i := range v {
+		v[i] = float64(i)
+	}
+
+	// Create matrix view of vector
+	m := v.Matrix(rows, columns)
+
+	// If number of rows of matrix is not correct
+	if act, exp := len(m), rows; act != exp {
+		t.Fatalf("len(m) = %v, expected %v", act, exp)
+	}
+
+	// If number of columns of matrix is not correct
+	if act, exp := len(m[0]), columns; act != exp {
+		t.Fatalf("len(m[0]) = %v, expected %v", act, exp)
+	}
+
+	// Loop through matrix in row-major order
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+
+			// Verify that matrix values match corresponding vector values
+			if act, exp := m[i][j], v[i*columns+j]; act != exp {
+				t.Fatalf("m[i][j] = %v, expected %v", act, exp)
+			}
+		}
+	}
+}
+
 func TestVector_Fill(t *testing.T) {
 	const value = 1.0
 	v := NewVector(5)
