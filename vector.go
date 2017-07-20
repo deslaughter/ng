5cc2a79eae0fbf9f9b37289e6c53c01165d173c9
Derek Slaughter <deslaughter@gmail.com>
2017-07-19 21:40:22 -0400

    Renamed Vector method receiver to match math notation

--- vector.go
+++ vector.go
@@ -19,8 +19,8 @@ func NewVector(size int) Vector {
 // Matrix returns a 2-D view of the vector with the given number of rows and
 // columns. The vector is interpreted in row-major order. A panic will occur
 // if rows * columns > Vector.Size().
-func (v Vector) Matrix(rows, columns int) Matrix {
-	d := v[:rows*columns]
+func (a Vector) Matrix(rows, columns int) Matrix {
+	d := a[:rows*columns]
 	m := make([][]float64, rows)
 	for i := range m {
 		m[i], d = d[:columns:columns], d[columns:]
@@ -29,50 +29,50 @@ func (v Vector) Matrix(rows, columns int) Matrix {
 }
 
 // Dimensions returns the number of dimensions in the vector which is 1.
-func (v Vector) Dimensions() int {
+func (a Vector) Dimensions() int {
 	return 1
 }
 
 // Size returns of slice of integers containing the length of each dimension
 // of the vector. Since there is only one dimension, the first element of
 // the slice will contain the same value as Vector.Size().
-func (v Vector) Size() []int {
-	return []int{len(v)}
+func (a Vector) Size() []int {
+	return []int{len(a)}
 }
 
 // Dot returns the dot product of this vector and the given vector.
-// This function will panic if vn.Size() < v.Size().
-func (v Vector) Dot(vn Vector) float64 {
+// This function will panic if b.Size() < a.Size().
+func (a Vector) Dot(b Vector) float64 {
 	sum := 0.0
-	vt := vn[:len(v)]
-	for i := range v {
-		sum += v[i] * vt[i]
+	bt := b[:len(a)]
+	for i := range a {
+		sum += a[i] * bt[i]
 	}
 	return sum
 }
 
 // Magnitude returns the sum of each element squared.
-func (v Vector) Magnitude() float64 {
+func (a Vector) Magnitude() float64 {
 	sum := 0.0
-	for i := range v {
-		sum += v[i] * v[i]
+	for i := range a {
+		sum += a[i] * a[i]
 	}
 	return math.Sqrt(sum)
 }
 
 // Scale multiplies all elements of the vector by the given value.
-func (v Vector) Scale(s float64) {
-	for i := range v {
-		v[i] *= s
+func (a Vector) Scale(s float64) {
+	for i := range a {
+		a[i] *= s
 	}
 }
 
 // Add sums each element of the given vector with the current vector and stores
 // the resulting value in the current vector.
-func (v Vector) Add(vn Vector) {
-	vt := vn[:len(v)]
-	for i := range v {
-		v[i] += vt[i]
+func (a Vector) Add(b Vector) {
+	bt := b[:len(a)]
+	for i := range a {
+		a[i] += bt[i]
 	}
 }
 
@@ -83,25 +83,25 @@ func (v Vector) Normalize() {
 }
 
 // Fill sets all elements of the vector to the given value.
-func (v Vector) Fill(value float64) {
-	for i := range v {
-		v[i] = value
+func (a Vector) Fill(v float64) {
+	for i := range a {
+		a[i] = v
 	}
 }
 
 // Resize changes the size and capacity of the vector. If the capacity is
 // less than size, a new vector is allocated and the values are copied into it.
-func (v *Vector) Resize(size int) {
-	if size <= cap(*v) {
-		*v = (*v)[:size:size]
+func (a *Vector) Resize(size int) {
+	if size <= cap(*a) {
+		*a = (*a)[:size:size]
 	}
-	*v = append(*v, make([]float64, size-cap(*v))...)
+	*a = append(*a, make([]float64, size-cap(*a))...)
 }
 
 // Sum returns the sum of all values in the vector.
-func (v Vector) Sum() float64 {
+func (a Vector) Sum() float64 {
 	sum := 0.0
-	for _, value := range v {
+	for _, value := range a {
 		sum += value
 	}
 	return sum
